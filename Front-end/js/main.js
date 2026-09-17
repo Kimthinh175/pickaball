63bb2dc2651a82bcee071dea2ecb6f11daa2151d
Kim Thinh <kimthinh175@gmail.com>
2026-09-18 01:28:58 +0700

    chore: bump cache busting query strings to v21

--- Front-end/js/main.js
+++ Front-end/js/main.js
@@ -3,9 +3,9 @@
 // ==========================================
 
 import { calculateTournamentStatus, toast, svgAvatar, avatarOf, fmtDate } from './core/api.js?v=18';
-import { fetchRanking, renderMorePlayers, openPlayerProfile, closePlayerProfileModal, goToRankingPage, openAvatarZoom, closeAvatarZoom, exportLeaderboardPoster } from './modules/ranking.js?v=20';
+import { fetchRanking, renderMorePlayers, openPlayerProfile, closePlayerProfileModal, goToRankingPage, openAvatarZoom, closeAvatarZoom, exportLeaderboardPoster } from './modules/ranking.js?v=21';
 import { fetchTournaments } from './modules/tournaments.js?v=18';
-import { fetchTournamentDetail } from './modules/tournament-detail.js?v=20';
+import { fetchTournamentDetail } from './modules/tournament-detail.js?v=21';
 import { initSlider, goToSlide, nextSlide, prevSlide } from './modules/slider.js?v=18';
 
 // Expose handlers to window for inline HTML onclick attributes
